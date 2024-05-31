4a8cc4f1125545d54489d93a5032c35e40316373
Mikhail Kasimov <nsmikawork@gmail.com>
2024-05-31 21:04:05 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+
+    private Map<String, String> contacts = new HashMap<>();
+    public static int add(String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
 
+    @Test
+    void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        int result = phoneBook.add("Dolg", "88005553535");
+        assertEquals(1, result);
+    }
 }
