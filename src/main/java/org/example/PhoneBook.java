9070bd4ac3d915f2943277c6b48fe1cb6faf119d
Mikhail Kasimov <nsmikawork@gmail.com>
2024-05-31 21:22:12 +0300

    Stub and tests for findByNumber

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -12,4 +12,7 @@ public class PhoneBook {
         }
         return contacts.size();
     }
+    public String findByNumber(String number) {
+        return null;
+    }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -12,4 +12,11 @@ class PhoneBookTest {
         assertEquals(1, phoneBook.add("Dolg", "88005553535"));
         assertEquals(2, phoneBook.add("Bank", "900"));
     }
+
+    @Test
+    void testFindByNumber() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Dolg", "88005553535");
+        assertEquals("Dolg", phoneBook.findByNumber("88005553535"));
+    }
 }
