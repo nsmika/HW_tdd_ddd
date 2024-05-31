cdd98a008b0d8edc8a7e76cd1712855fc0ce5aac
Mikhail Kasimov <nsmikawork@gmail.com>
2024-05-31 21:07:47 +0300

    Stub and tests for add

--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -9,7 +9,6 @@ class PhoneBookTest {
     @Test
     void testAdd() {
         PhoneBook phoneBook = new PhoneBook();
-        int result = phoneBook.add("Dolg", "88005553535");
-        assertEquals(1, result);
+        assertEquals(1, phoneBook.add("Dolg", "88005553535"));
     }
 }
