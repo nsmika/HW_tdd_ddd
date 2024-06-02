dc858b01a10d75943fc8306448a7a973ae16a3de
Mikhail Kasimov <nsmikawork@gmail.com>
2024-06-02 11:05:13 +0300

    Stub and tests for findByName

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -20,4 +20,7 @@ public class PhoneBook {
         }
         return null;
     }
+    public String findByName(String name) {
+        return null;
+    }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -19,4 +19,11 @@ class PhoneBookTest {
         phoneBook.add("Dolg", "88005553535");
         assertEquals("Dolg", phoneBook.findByNumber("88005553535"));
     }
+
+    @Test
+    void testFindByName() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Dolg", "88005553535");
+        assertEquals("88005553535", phoneBook.findByName("Dolg"));
+    }
 }
